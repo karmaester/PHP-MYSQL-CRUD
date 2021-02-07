c7a3c7c216943779149655647b4ac853e8f60553
Khristian Rojas <khristianrojas15@gmail.com>
2021-02-07 19:08:08 -0300

    Add js validation for input lenght

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
     <title>PHP/MYSQL</title>
 </head>
 <body>
+<script>
+function validar(element)
+{
+const min=parseInt(element.dataset.lengthmin);
+if(element.value.length>=min)
+{
+
+element.classList.remove("borderRed");
+return true;
+}else{
+
+alert("Debe ingresar el minimo de caracteres indicados");
+element.classList.add("borderRed");
+return false;
+}
+}
+
+function validarFormulario(formulario)
+{
+let result=true;
+const input=formulario.querySelectorAll("input[type=text]");
+// recorremos todos los input para ver si algun input no cumple con los caracteres minimos
+for (i of input) {
+if (validar(i)==false) {
+result=false;
+}
+}
+return result;
+}
+</script>
+<style>
+.borderGrey {border:2px solid Grey;}
+.borderRed {border-color: Red;}
+</style>
 <nav class="navbar navbar-dark bg-dark">
     <div class="container">
         <a href="index.php" class="navbar-brand">
--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <form action="save_car.php" method="POST">
                     <div class="form-group">
                         <label for="register">Register</label>
-                        <input type="text" name="register" class="form-control" placeholder="Enter value" autofocus>
+                        <input type="text" name="register" class="form-control" onkeyup=validar(this); data-lengthMin="6" placeholder="Enter value" autofocus>
                         <label for="serial_engine">Engine Serial</label>
                         <input type="text" name="serial_engine" class="form-control" placeholder="Enter value">
                         <label for="serial_chassis">Chassis Serial</label>
@@ -27,6 +27,8 @@
                         <input type="text" name="brand" class="form-control" placeholder="Enter brand">
                         <label for="model">Model</label>
                         <input type="text" name="model" class="form-control" placeholder="Enter model">
+                        <label for="year">Year</label>
+                        <input type="text" name="year" class="form-control" onkeyup=validar(this); data-lengthMin="4" placeholder="Enter year">
                         <label for="color">Color</label>
                         <input type="text" name="color" class="form-control" placeholder="Enter color">
                         <label for="price">Price</label>
@@ -40,28 +42,38 @@
             <table class="table table-bordered">
                 <thead>
                 <tr>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Created At</th>
+                    <th>Register</th>
+                    <th>Engine Serial</th>
+                    <th>Chassis Serial</th>
+                    <th>Brand</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Color</th>
+                    <th>Price</th>
                     <th>Action</th>
                 </tr>
                 </thead>
                 <tbody>
 
                 <?php
-                $query = "SELECT * FROM task";
-                $result_tasks = mysqli_query($conn, $query);    
+                $query = "SELECT * FROM cars";
+                $result_cars = mysqli_query($conn, $query);    
 
-                while($row = mysqli_fetch_assoc($result_tasks)) { ?>
+                while($row = mysqli_fetch_assoc($result_cars)) { ?>
                 <tr>
-                    <td><?php echo $row['title']; ?></td>
-                    <td><?php echo $row['description']; ?></td>
-                    <td><?php echo $row['created_at']; ?></td>
+                    <td><?php echo $row['register']; ?></td>
+                    <td><?php echo $row['serial_engine']; ?></td>
+                    <td><?php echo $row['serial_chassis']; ?></td>
+                    <td><?php echo $row['brand']; ?></td>
+                    <td><?php echo $row['model']; ?></td>
+                    <td><?php echo $row['year']; ?></td>
+                    <td><?php echo $row['color']; ?></td>
+                    <td><?php echo $row['price']; ?></td>
                     <td>
                     <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
                         <i class="fas fa-marker"></i>
                     </a>
-                    <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                    <a href="delete_car.php?id=<?php echo $row['id']?>" class="btn btn-danger">
                         <i class="far fa-trash-alt"></i>
                     </a>
                     </td>
